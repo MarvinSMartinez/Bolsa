71e65e877d1a2f06b1e007d4170d9ffcce7cf124
MarvinSMartinez <marvin.s.martinez.h@gmail.com>
2022-05-22 15:26:55 -0600

    Pantalla Empresa 2

---
+++ app/src/main/java/com/g06/bolsa/EmpresaEditarActivity.java
@@ -0,0 +1,14 @@
+package com.g06.bolsa;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EmpresaEditarActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_empresa_editar);
+    }
+}
