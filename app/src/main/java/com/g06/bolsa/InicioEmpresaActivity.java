0ec69f6aa059c2e646de2991001b16698aa5ac38
MarvinSMartinez <marvin.s.martinez.h@gmail.com>
2022-05-22 01:00:23 -0600

    Pantalla Empresa

--- app/src/main/java/com/g06/bolsa/InicioEmpresaActivity.java
+++ app/src/main/java/com/g06/bolsa/InicioEmpresaActivity.java
@@ -3,8 +3,8 @@ package com.g06.bolsa;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-
-
+import android.view.View;
+import android.widget.Toast;
 
 
 public class InicioEmpresaActivity extends AppCompatActivity {
@@ -14,4 +14,15 @@ public class InicioEmpresaActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inicio_empresa);
     }
+
+    public void botonPerfil(View v){
+        Toast.makeText(this, "Perfil de empresa", Toast.LENGTH_SHORT).show();
+        setContentView(R.layout.activity_empresa_editar);
+    }
+
+    public void botonOfertas(View v){
+        Toast.makeText(this, "Ofertas de empresa", Toast.LENGTH_SHORT).show();
+        setContentView(R.layout.activity_menu_ofertas);
+    }
+
 }
