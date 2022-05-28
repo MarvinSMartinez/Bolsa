64dffbae7b111dfbb6b37b2ed7c135aa1a5f4714
xdxmg <xdxmg15@gmail.com>
2022-05-27 23:45:28 -0600

    departamento

--- app/src/main/java/com/g06/bolsa/departamento/DepartamentoActualizarActivity.java
+++ app/src/main/java/com/g06/bolsa/departamento/DepartamentoActualizarActivity.java
@@ -3,14 +3,48 @@ package com.g06.bolsa.departamento;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import com.g06.bolsa.ControlBDLJ16001;
 import com.g06.bolsa.R;
+import com.g06.bolsa.departamento.ControlDepartamento;
+import com.g06.bolsa.clases_auxiliares.Departamento;
 
 public class DepartamentoActualizarActivity extends AppCompatActivity {
+    ControlDepartamento DBHelper;
+    EditText idDepartamento;
+    EditText nombreDepartamento;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_departamento_actualizar);
+        DBHelper = new ControlDepartamento(this);
+
+        idDepartamento = findViewById(R.id.editIdDepartamento);
+        nombreDepartamento = findViewById(R.id.editNombreDepartamento);
+
+    }
+
+    public void actualizarDepartamento(View v) {
+        Departamento departamento = new Departamento();
+
+        departamento.setId(idDepartamento.getText().toString());
+        departamento.setNombre(nombreDepartamento.getText().toString());
+
+        DBHelper.abrir();
+        String estado = DBHelper.ActualizarDepartamento(departamento);
+        DBHelper.cerrar();
+
+        Toast.makeText(this, estado, Toast.LENGTH_SHORT).show();
+    }
+
+    public void limpiarTexto(View v) {
+        idDepartamento.setText("");
+        nombreDepartamento.setText("");
+
     }
 }
--- app/src/main/java/com/g06/bolsa/departamento/DepartamentoEliminarActivity.java
+++ app/src/main/java/com/g06/bolsa/departamento/DepartamentoEliminarActivity.java
@@ -2,15 +2,40 @@ package com.g06.bolsa.departamento;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import com.g06.bolsa.ControlBDLJ16001;
 import com.g06.bolsa.R;
+import com.g06.bolsa.clases_auxiliares.ContactoAspirante;
+import com.g06.bolsa.clases_auxiliares.Departamento;
 
-public class DepartamentoEliminarActivity extends AppCompatActivity {
+public class DepartamentoEliminarActivity extends Activity {
+    ControlDepartamento DBHelper;
+    EditText idDepartamento;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_departamento_eliminar);
+        DBHelper = new ControlDepartamento(this);
+
+        idDepartamento = findViewById(R.id.editEliminarIdDepartamento);
+    }
+
+    public void eliminarDepartamento(View v) {
+        String regEliminadas;
+        Departamento departamento = new Departamento();
+        departamento.setId(idDepartamento.getText().toString());
+
+        DBHelper.abrir();
+        regEliminadas = DBHelper.eliminarDepartamento(departamento);
+        DBHelper.cerrar();
+
+        Toast.makeText(this, regEliminadas, Toast.LENGTH_SHORT).show();
     }
 }
