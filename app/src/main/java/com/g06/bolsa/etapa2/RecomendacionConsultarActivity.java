aefc2f9f7a3a0d67b260f1bd1193f0b1ab923218
xdxmg <xdxmg15@gmail.com>
2022-05-30 21:37:06 -0600

    fix lo que dice etapa2

--- app/src/main/java/com/g06/bolsa/etapa2/RecomendacionConsultarActivity.java
+++ app/src/main/java/com/g06/bolsa/etapa2/RecomendacionConsultarActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.g06.bolsa.R;
+
 public class RecomendacionConsultarActivity extends AppCompatActivity {
 
     @Override
